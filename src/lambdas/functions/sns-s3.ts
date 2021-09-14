2a853087c3348e1c35e8ffdbc850ca7fc21dfb4a
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2021-09-14 21:05:46 +1200

    record lambda complete

--- src/lambdas/functions/sns-s3.ts
+++ src/lambdas/functions/sns-s3.ts
@@ -6,6 +6,9 @@ const s3 = new S3({
   region: 'ap-southeast-2'
 })
 
+const bucket = 'jvb-code'
+const key = 'email-data/all.json'
+
 export const handler = async (event: SNSEvent): Promise<void> => {
   try {
     console.log('-------------')
@@ -15,6 +18,7 @@ export const handler = async (event: SNSEvent): Promise<void> => {
       `records.length=${event.Records.length}`
     )
 
+    const incomingRecords: EmailRequest[] = []
     for(let i = 0; i < event.Records.length; i++) {
       const record = event.Records[i]
       console.log(
@@ -22,7 +26,50 @@ export const handler = async (event: SNSEvent): Promise<void> => {
         record.Sns.Message
       )
       const data: EmailRequest = JSON.parse(record.Sns.Message)
+      const request = new EmailRequest(data)
+      if (request.isValid) {
+        incomingRecords.push(data)
+      }
     }
+
+    let currentRecords: EmailRequest[] = []
+    try {
+      console.log(
+        's3.getObject',
+        `bucket=${bucket}`,
+        `key=${key}`
+      )
+      const obj = await s3.getObject({
+        Bucket: bucket,
+        Key: key
+      }).promise()
+      currentRecords = JSON.parse(obj.Body.toString()) as EmailRequest[]
+    } catch {
+      console.error('failed to load current records from json file')
+      console.error(
+        `bucket=${bucket}`,
+        `key=${key}`
+      )
+    }
+    console.log(
+      `currentRecords.length=${currentRecords.length}`
+    )
+    const allRecords = [...currentRecords, ...incomingRecords]
+    console.log(
+      `allRecords.length=${allRecords.length}`
+    )
+    console.log(
+      's3.putObject',
+      `bucket=${bucket}`,
+      `key=${key}`
+    )
+    await s3.putObject({
+      Bucket: bucket,
+      Key: key,
+      Body: JSON.stringify(allRecords),
+      ContentType: 'application/json'
+    }).promise()
+    console.log('sns-s3.handler success')
   } catch (e) {
     console.error(e)
     console.error('sns-s3.handler failed')
--- src/lambdas/models/emailRequest.ts
+++ src/lambdas/models/emailRequest.ts
@@ -1,7 +1,18 @@
-export interface EmailRequest {
+export class EmailRequest {
   templateId: string
   email: string
   sendAt: number
   subject: string
   bodyText: string
+  constructor(data: EmailRequest) {
+    this.templateId = data.templateId
+    this.email = data.email
+    this.sendAt = data.sendAt
+    this.subject = data.subject
+    this.bodyText = data.bodyText
+  }
+
+  get isValid(): boolean {
+    return !!this.email
+  }
 }
